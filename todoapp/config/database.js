4234d9fe2d45b863e8bc9c0318b8a86dfa8c70f9
Prachi Polakhare <prachi24599@gmail.com>
2024-05-17 23:38:18 +0530

    DB connection using mongoose

--- todoapp/config/database.js
+++ todoapp/config/database.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
+//To access enviornment variables from .env file
+require('dotenv').config()
 
-const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL);
-}
+const dbConnect = () => (mongoose.connect(process.env.DATABASE_URL))
+.then(()=> {console.log("DB connection successful!")})
+.catch((error) => {
+    console.log("Issue in DB connection!");
+    console.error(err);
+    process.exit(1);
+})
+
+export default dbConnect;
