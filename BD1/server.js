bfe0b59fb429258b6d3fead7a7793fa33999c737
Prachi Polakhare <prachi24599@gmail.com>
2024-06-14 00:07:35 +0530

    using bodyParser to parse json data

--- BD1/server.js
+++ BD1/server.js
@@ -2,13 +2,18 @@
 // 2. move into that folder
 // 3. npm init -y
 // 4. open folder using VSCode
-// 5. npm i express
+// 5. npm i express - downloads the node packages required for express
 // 6. create server.js
 // 7. RUN - node server.js
 
 //Server Instantiate
 const express = require('express');
 const app = express();
+
+//Used to parse req.body in express
+const bodyParser = require("body-parser");
+//Specifically parse json data and add it to request.body object -> mainly used in case of PUT or POST
+app.use(bodyParser.json())
 
 //activate server on 3000 port
 app.listen(5000, () => {
