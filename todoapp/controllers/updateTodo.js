e01a86d9a74a77a5f1193b9c43ddb003001f7b0d
Prachi Polakhare <prachi.polakhare@htstechsolutions.com>
2024-06-18 17:10:59 +0530

    updateTodo functionality added

---
+++ todoapp/controllers/updateTodo.js
@@ -0,0 +1,33 @@
+//import the model
+import Todo from "../models/Todo.js"
+
+//Define route handler
+const updateTodo = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const {title, description} = req.body;
+
+        const todo = await Todo.findByIdAndUpdate(
+            {_id : id},
+            {title, description, updatedAt : Date.now()}
+        )
+
+        res.status(200).json({
+            success : true,
+            data : todo,
+            message :  `Record updated successfully`
+        })
+    }
+    catch(err) {
+        console.error(err);
+        console.log(err);
+        res.status(500).json({
+            success : false,
+            data : "Internal server error",
+            message : err.message,
+        })
+    }
+}   
+
+// Export the createTodo function
+export default updateTodo;
--- todoapp/routes/Todo.js
+++ todoapp/routes/Todo.js
@@ -3,10 +3,12 @@ const router = express.Router();
 //Import Controller
 import createTodo from "../controllers/createTodo.js";
 import {getTodo, getTodoById} from "../controllers/getTodo.js";
+import updateTodo from "../controllers/updateTodo.js";
 
 //define API routes
 router.post("/createTodo", createTodo); 
 router.get("/getTodos", getTodo);
 router.get("/getTodos/:id", getTodoById);
+router.put("/updateTodo/:id", updateTodo);
 
 export default router;
