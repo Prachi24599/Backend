c49db434cf46b1bdac000cf4b89668f34fac143e
Prachi Polakhare <prachi.polakhare@htstechsolutions.com>
2024-06-18 16:49:19 +0530

    created controller for getTodos by Id

--- todoapp/controllers/getTodo.js
+++ todoapp/controllers/getTodo.js
@@ -25,5 +25,36 @@ const getTodo = async(req, res) => {
     }
 }   
 
+const getTodoById = async (req, res) => {
+    try{
+        //extract todo items based on id
+        const id = req.params.id;
+        const todo = await Todo.find({_id : id})
+
+        //data for given id not found
+        if(!todo){
+            return res.status(404).json({
+                success : false,
+                message : "No data found with given id"
+            })
+        }
+        //data for given id is found
+        res.status(200).json({
+            success : true,
+            data : todo,
+            message :  `Todo ${id} data is successfully fetched`
+        })
+    }   
+    catch{
+        console.error(err);
+        console.log(err);
+        res.status(500).json({
+            success : false,
+            error : err?.message,
+            message : "Server Error"
+        })
+    }
+}
+
 // Export the getTodo function
-export default getTodo;
+export default {getTodo, getTodoById};
--- todoapp/routes/Todo.js
+++ todoapp/routes/Todo.js
@@ -2,10 +2,11 @@ import express from 'express';
 const router = express.Router();
 //Import Controller
 import createTodo from "../controllers/createTodo.js";
-import getTodo from "../controllers/getTodo.js";
+import {getTodo, getTodoById} from "../controllers/getTodo.js";
 
 //define API routes
 router.post("/createTodo", createTodo); 
 router.get("/getTodos", getTodo);
+router.get("/getTodos/:id", getTodoById);
 
 export default router;
