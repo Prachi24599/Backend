f3048b246670e06986082fe7f8ae9ab3e36660a8
Prachi Polakhare <prachi.polakhare@htstechsolutions.com>
2024-06-19 16:56:25 +0530

    commentModel added

---
+++ blogApp/models/commentModel.js
@@ -0,0 +1,24 @@
+// In comment model, we have 3 things
+// - The post on which comment added (id of that post)
+// - Who has added the comment (user id)
+// - Body of the comment
+
+import mongoose from "mongoose";
+
+//route handler
+const commentSchema = new mongoose.Schema({
+    post : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Post" //reference to post model
+    },
+    user : {
+        type : String,
+        required : true
+    },
+    body : {
+        type : String,
+        required : true
+    }
+})
+
+module.exports = mongoose.model("Comment", commentSchema);
