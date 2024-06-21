863e763aa4c0ba1c60eaf34ebea60534fc1cef03
Prachi Polakhare <prachi24599@gmail.com>
2024-06-22 00:19:51 +0530

    added middleware

--- backendFirstApp/index.js
+++ backendFirstApp/index.js
@@ -5,3 +5,7 @@ port = 3000;
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 })
+
+//middleware - It is used to log incoming request, authenticate, parse data, error handing
+// express.json() - parse json data
+app.use(express.json())
